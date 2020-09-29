85de957522a0f5f0854a00083bf32f446b2ab50a
Isaac Gorman <emailme@isaacg.net>
2020-09-28 19:40:11 -0500

    init commit for team web app

---
+++ prj/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h4>The Dream Team</h4>
+      </header>
+    </div>
+  );
+}
+export default App;
